03c2533b63f1d71882b84ffed0a7d3aa03e410ca
Heitor Mancini <159497313+HeitorMancin@users.noreply.github.com>
2025-04-01 13:24:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,22 @@ dados = "https://raw.githubusercontent.com/HeitorMancin/PrototipoSAC/refs/heads/
 df = pd.read_excel(dados)
 
 st.set_page_config(layout="wide")
+col1, col2 = st.columns([1, 1])  # Duas colunas de tamanho igual
+
+# Logotipo na primeira coluna (alinhada à esquerda)
+with col1:
+    st.image("https://www.biolabfarma.com.br/wp-content/themes/biolabtheme/assets/images/logo-menu.png", width=150)
+
+# TAG 'Viva a Evolução' na segunda coluna (alinhada à direita)
+with col2:
+    st.markdown(
+        """
+        <div style="display: flex; justify-content: flex-end;">
+            <img src="https://www.biolabfarma.com.br/wp-content/themes/biolabtheme/assets/images/tagline-viva-evolucao.png" style="max-width: 100%; height: auto;">
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 # Código HTML e CSS para a faixa no cabeçalho
 html_code = """
@@ -36,8 +52,6 @@ components.html(html_code, height=50)
 
 #Visualização da tabela toda
 df
-
-d = st.sidebar.date_imput("Selecione a data das transcrições", datetime.date)
 
 # Agrupa os dados por atendente e sentimento e conta as ocorrências
 sentimentos_por_atendente = df.groupby(['atendente', 'sentimento']).size().reset_index(name='contagem')
