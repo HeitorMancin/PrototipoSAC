1b4a20c4c595dd14c875ba9f64f22ea6984af49c
Heitor Mancini <159497313+HeitorMancin@users.noreply.github.com>
2025-04-01 11:05:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,8 @@ df
 # Agrupa os dados por atendente e sentimento e conta as ocorrências
 sentimentos_por_atendente = df.groupby(['atendente', 'sentimento']).size().reset_index(name='contagem')
 
-# Cria o gráfico de barras
+'''
+# Cria o simples gráfico de barras
 fig, ax = plt.subplots(figsize=(16, 6))  # Cria a figura e os eixos
 sns.barplot(x='atendente', y='contagem', hue='sentimento', data=sentimentos_por_atendente, ax=ax) # passa os eixos para o seaborn
 ax.grid(color='gray', linestyle='--', linewidth=0.5)
@@ -64,7 +65,8 @@ ax.set_xlabel('Atendente')
 ax.set_ylabel('Contagem')
 plt.xticks(rotation=45, ha='right')  # Rotaciona os rótulos do eixo x para melhor legibilidade
 plt.tight_layout()  # Ajusta o layout para evitar sobreposição de elementos
-#st.pyplot(fig) #passa a figura para o Streamlit
+st.pyplot(fig) #passa a figura para o Streamlit
+'''
 
 # Carregamento e pré-processamento de dados
 df = pd.read_excel(dados, engine='openpyxl')  # Substitua "DF.xlsx" pelo caminho real do seu arquivo
@@ -76,19 +78,21 @@ def plot_filtered_sentiments(atendente, sentimentos):
     df_atendente = df_filtrado[df_filtrado['atendente'] == atendente]
     filtered_data = df_atendente[df_atendente['sentimento'].isin(sentimentos)]
 
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(12, 6))
+    sns.set_style('whitegrid')
+    cores = sns.color_palette('pastel')
 
     if len(filtered_data) == 0:
         filtered_data = pd.DataFrame({'sentimento': sentimentos, 'count': [0] * len(sentimentos)})
-        sns.barplot(x='sentimento', y='count', data=filtered_data, palette='Dark2')
+        sns.barplot(x='sentimento', y='count', data=filtered_data, palette=cores)
     else:
-        sns.countplot(x='sentimento', data=filtered_data, palette='Dark2')
+        sns.countplot(x='sentimento', data=filtered_data, palette=cores)
 
-    plt.title(f"Sentimentos do Atendente: {atendente} (Filtrado)")
-    plt.xlabel("Sentimento")
-    plt.ylabel("Contagem")
+    plt.title(f"Sentimentos do Atendente: {atendente} (Filtrado)", fontsize=16, fontweight='bold')
+    plt.xlabel("Sentimento", fontsize=12)
+    plt.ylabel("Contagem", fontsize=12)
     plt.gca().spines[['top', 'right']].set_visible(False)
-    plt.xticks(rotation=45, ha='right') # Rotação dos rótulos do eixo x
+    plt.xticks(rotation=45, ha='right', fontsize=10) # Rotação dos rótulos do eixo x
 
     plt.tight_layout()
     st.pyplot(plt)  # Exibir o gráfico no Streamlit
@@ -108,5 +112,3 @@ sentimentos_selecionados = st.multiselect("Selecione sentimentos:", todos_sentim
 # Botão para gerar o gráfico
 if st.button("Gerar Gráfico"):
     plot_filtered_sentiments(atendente_selecionado, sentimentos_selecionados)
-
-
