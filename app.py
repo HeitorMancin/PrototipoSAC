d03b36c5cc28967e23f4aca1a501b2691de20de8
Heitor Mancini <159497313+HeitorMancin@users.noreply.github.com>
2025-04-01 10:46:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -56,15 +56,15 @@ df
 sentimentos_por_atendente = df.groupby(['atendente', 'sentimento']).size().reset_index(name='contagem')
 
 # Cria o gráfico de barras
-plt.figure(figsize=(16, 6))  # Ajusta o tamanho da figura
-sns.barplot(x='atendente', y='contagem', hue='sentimento', data=sentimentos_por_atendente)
-plt.grid(color='gray', linestyle='--', linewidth=0.5)
-plt.title('Sentimento por Atendente')
-plt.xlabel('Atendente')
-plt.ylabel('Contagem')
+fig, ax = plt.subplots(figsize=(16, 6))  # Cria a figura e os eixos
+sns.barplot(x='atendente', y='contagem', hue='sentimento', data=sentimentos_por_atendente, ax=ax) # passa os eixos para o seaborn
+ax.grid(color='gray', linestyle='--', linewidth=0.5)
+ax.set_title('Sentimento por Atendente')
+ax.set_xlabel('Atendente')
+ax.set_ylabel('Contagem')
 plt.xticks(rotation=45, ha='right')  # Rotaciona os rótulos do eixo x para melhor legibilidade
 plt.tight_layout()  # Ajusta o layout para evitar sobreposição de elementos
-st.pyplot(fig)
+st.pyplot(fig) #passa a figura para o Streamlit
 
 
 
